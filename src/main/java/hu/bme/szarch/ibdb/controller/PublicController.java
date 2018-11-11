df17ebea1400e289519a126ea7bc305f9458db61
Nagy László <nagylaszlo9423@gmail.com>
2018-11-12 00:24:04 +0100

    Changes

---
+++ src/main/java/hu/bme/szarch/ibdb/controller/PublicController.java
@@ -0,0 +1,39 @@
+package hu.bme.szarch.ibdb.controller;
+
+import hu.bme.szarch.ibdb.controller.dto.DtoMapper;
+import hu.bme.szarch.ibdb.controller.dto.book.BookResponse;
+import hu.bme.szarch.ibdb.service.BookService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.OffsetDateTime;
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+
+    private BookService bookService;
+
+    public PublicController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/popular")
+    public Page<BookResponse> getPopularBooks() {
+        return DtoMapper.bookResultsToResponse(bookService.getPopulars(PageRequest.of(0, 10)));
+    }
+
+    @GetMapping("/bestseller")
+    public Page<BookResponse> getBestSellerBooks() {
+        return DtoMapper.bookResultsToResponse(bookService.getBestSellers(PageRequest.of(0, 10)));
+    }
+
+    @GetMapping("/trending")
+    public Page<BookResponse> getTrendingBooks() {
+        return DtoMapper.bookResultsToResponse(bookService.getTrending(PageRequest.of(0, 10), OffsetDateTime.now().minusMonths(3)));
+    }
+
+}
